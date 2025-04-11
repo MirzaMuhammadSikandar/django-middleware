f91ea0903584fa3be3ba8e976729fafdd2703c30
Sikandar <sikandarmiirza@gmail.com>
2025-04-11 07:44:20 -0400

    testing files url updated

--- api/middleware/disable_csrf.py
+++ api/middleware/disable_csrf.py
@@ -4,3 +4,4 @@ class DisableCSRFMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.path in ['/api/users/register/', '/api/users/login/']:
             setattr(request, '_dont_enforce_csrf_checks', True)
+            
